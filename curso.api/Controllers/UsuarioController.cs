c1837e4c41ba8147d41765f5575a0946fbc57ced
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2021-07-08 11:05:45 -0300

    Modelagem e roteamento de login e registro

---
+++ curso.api/Controllers/UsuarioController.cs
@@ -0,0 +1,24 @@
+using curso.api.Models.Usuarios;
+using Microsoft.AspNetCore.Mvc;
+
+namespace curso.api.Controllers
+{
+  [Route("api/v1/usuario")]
+  [ApiController]
+  public class UsuarioController : ControllerBase
+  {
+    [HttpPost]
+    [Route("logar")]
+    public IActionResult Logar(LoginViewModelInput loginViewModelInput)
+    {
+      return Ok(loginViewModelInput);
+    }
+
+    [HttpPost]
+    [Route("registrar")]
+    public IActionResult Registrar(RegistroViewModelInput loginViewModelInput)
+    {
+      return Created("", loginViewModelInput);
+    }
+  }
+}
---
+++ curso.api/Models/Usuarios/LoginViewModelInput.cs
@@ -0,0 +1,8 @@
+namespace curso.api.Models.Usuarios
+{
+  public class LoginViewModelInput
+  {
+    public string Login { get; set; }
+    public string Senha { get; set; }
+  }
+}
