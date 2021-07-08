37993d297c418f53d2cd6ccb5bd9f0f4e558df23
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2021-07-08 15:24:17 -0300

    Configuração do JWT

--- curso.api/Controllers/UsuarioController.cs
+++ curso.api/Controllers/UsuarioController.cs
@@ -1,34 +1,76 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using curso.api.Filters;
 using curso.api.Models;
 using curso.api.Models.Usuarios;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using Swashbuckle.AspNetCore.Annotations;
 
 namespace curso.api.Controllers
 {
-  [Route("api/v1/usuario")]
-  [ApiController]
-  public class UsuarioController : ControllerBase
-  {
-    //   <sumary>
-    //   Este serviço permite autenticar um usuário cadastrado e ativo.
-    //   </sumary>
-    //   <param name="loginViewModelInput">View Model do login</param>
-    //   <returns>Retorna status ok, dados do usuário e o token, em caso de sucesso.</returns>
-    [SwaggerResponse(statusCode: 200, description: "Sucesso ao autenticar", Type = typeof(LoginViewModelInput))]
-    [SwaggerResponse(statusCode: 400, description: "Campos obrigatórios", Type = typeof(ValidaCampoViewModelOutput))]
-    [SwaggerResponse(statusCode: 500, description: "Erro interno", Type = typeof(ErroGenericoViewModel))]
-    [HttpPost]
-    [Route("logar")]
-    public IActionResult Logar(LoginViewModelInput loginViewModelInput)
+    [Route("api/v1/usuario")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
     {
-      return Ok(loginViewModelInput);
-    }
+        //   <sumary>
+        //   Este serviço permite autenticar um usuário cadastrado e ativo.
+        //   </sumary>
+        //   <param name="loginViewModelInput">View Model do login</param>
+        //   <returns>Retorna status ok, dados do usuário e o token, em caso de sucesso.</returns>
 
-    [HttpPost]
-    [Route("registrar")]
-    public IActionResult Registrar(RegistroViewModelInput loginViewModelInput)
-    {
-      return Created("", loginViewModelInput);
+
+        [SwaggerResponse(statusCode: 200, description: "Sucesso ao autenticar", Type = typeof(LoginViewModelInput))]
+        [SwaggerResponse(statusCode: 400, description: "Campos obrigatórios", Type = typeof(ValidaCampoViewModelOutput))]
+        [SwaggerResponse(statusCode: 500, description: "Erro interno", Type = typeof(ErroGenericoViewModel))]
+        [HttpPost]
+        [Route("logar")]
+        [ValidacaoModelStateCustomizado]
+        public IActionResult Logar(LoginViewModelInput loginViewModelInput)
+        {
+
+            var usuarioViewModelOutput = new UsuarioViewModelOutput()
+            {
+                Codigo = 1,
+                Login = "fernando.macedo",
+                Email = "fernando@macedo.com.br"
+            };
+
+            var secret = Encoding.ASCII.GetBytes("QhvE%pMvOsP1NHylHV#I5N^$RrJE6sa#lKK7k4ciGEnm3K9SFV");
+            var symmetricSecurityKey = new SymmetricSecurityKey(secret);
+            var securityTokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, usuarioViewModelOutput.Codigo.ToString()),
+                    new Claim(ClaimTypes.Name, usuarioViewModelOutput.Login.ToString()),
+                    new Claim(ClaimTypes.Email, usuarioViewModelOutput.Email.ToString()),
+                }),
+                Expires = DateTime.UtcNow.AddDays(1),
+                SigningCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256Signature)
+            };
+            var jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+            var tokenGenerated = jwtSecurityTokenHandler.CreateToken(securityTokenDescriptor);
+            var token = jwtSecurityTokenHandler.WriteToken(tokenGenerated);
+
+
+            return Ok(new
+            {
+                Token = token,
+                Usuario = usuarioViewModelOutput
+
+            });
+        }
+
+        [HttpPost]
+        [Route("registrar")]
+        [ValidacaoModelStateCustomizado]
+        public IActionResult Registrar(RegistroViewModelInput loginViewModelInput)
+        {
+            return Created("", loginViewModelInput);
+        }
     }
-  }
 }
--- curso.api/Startup.cs
+++ curso.api/Startup.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,55 +12,75 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 namespace curso.api
 {
-  public class Startup
-  {
-    public Startup(IConfiguration configuration)
+    public class Startup
     {
-      Configuration = configuration;
-    }
-
-    public IConfiguration Configuration { get; }
-
-    // This method gets called by the runtime. Use this method to add services to the container.
-    public void ConfigureServices(IServiceCollection services)
-    {
-
-      services.AddControllers();
-      services.AddSwaggerGen(c =>
-      {
-        c.SwaggerDoc("v1", new OpenApiInfo { Title = "curso.api", Version = "v1" });
-      });
-    }
-
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    {
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-        app.UseSwagger();
-        app.UseSwaggerUI(c =>
+        public Startup(IConfiguration configuration)
         {
-          c.SwaggerEndpoint("/swagger/v1/swagger.json", "API Curso");
-          c.RoutePrefix = string.Empty; //swagger
-        });
+            Configuration = configuration;
+        }
 
-      }
+        public IConfiguration Configuration { get; }
 
-      app.UseHttpsRedirection();
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
 
-      app.UseRouting();
+            services.AddControllers()
+              .ConfigureApiBehaviorOptions(options => options.SuppressModelStateInvalidFilter = true);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "curso.api", Version = "v1" });
+            });
+            var secret = Encoding.ASCII.GetBytes(Configuration.GetSection("JwtConfigurations:Secret").Value);
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(secret),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+        }
 
-      app.UseAuthorization();
+            // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+            public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+            {
+                if (env.IsDevelopment())
+                {
+                    app.UseDeveloperExceptionPage();
+                    app.UseSwagger();
+                    app.UseSwaggerUI(c =>
+                    {
+                        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API Curso");
+                        c.RoutePrefix = string.Empty; //swagger
+                    });
 
-      app.UseEndpoints(endpoints =>
-      {
-        endpoints.MapControllers();
-      });
+                }
+
+                app.UseHttpsRedirection();
+
+                app.UseRouting();
+
+                app.UseAuthorization();
+
+                app.UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllers();
+                });
+            }
+        }
     }
-  }
-}
