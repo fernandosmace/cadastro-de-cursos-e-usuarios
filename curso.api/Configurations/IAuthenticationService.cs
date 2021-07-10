7c06a811ed7666c094806f8d326149fde2d0c57c
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2021-07-10 15:19:33 -0300

    Criado serviço isolado para a autenticação JWT

---
+++ curso.api/Configurations/IAuthenticationService.cs
@@ -0,0 +1,9 @@
+using curso.api.Models.Usuarios;
+
+namespace curso.api.Configurations
+{
+  public interface IAuthenticationService
+  {
+    string GerarToken(UsuarioViewModelOutput usuarioViewModelOutput);
+  }
+}
