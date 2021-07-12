76868b5b4e5755ff48b86d222c7c0129779e7610
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2021-07-11 23:21:01 -0300

    Adicionda validação nos campos

--- curso.api/Models/Cursos/CursoViewModelInput.cs
+++ curso.api/Models/Cursos/CursoViewModelInput.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace curso.api.Controllers
 {
   public class CursoViewModelInput
   {
+    [Required(ErrorMessage = "O nome do curso é obrigatório!")]
     public string Nome { get; set; }
+
+    [Required(ErrorMessage = "A descriçaõ do curso é obrigatória!")]
     public string Descricao { get; set; }
   }
 }
