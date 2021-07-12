b3420a13bf767f27055fc2e2ed2d6a6fd41cdadc
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2021-07-12 00:28:12 -0300

    Refatoração dos testes

--- tests/curso.api.tests/Integrations/Controllers/CursoControllerTests.cs
+++ tests/curso.api.tests/Integrations/Controllers/CursoControllerTests.cs
@@ -1,91 +1,71 @@
+using AutoBogus;
+using curso.api.Controllers;
+using curso.api.Models.Cursos;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
-using AutoBogus;
-using curso.api.Controllers;
-using curso.api.Models.Cursos;
-using curso.api.tests.Configurations;
-using Microsoft.AspNetCore.Mvc.Testing;
-using Newtonsoft.Json;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace curso.api.tests.Integrations.Controllers
 {
     public class CursoControllerTests : UsuarioControllerTests
     {
-        protected CursoViewModelInput CursoViewModelInput;
-
         public CursoControllerTests(WebApplicationFactory<Startup> factory)
             : base(factory)
         {
         }
 
         [Fact]
-        public async Task Cadastrar_InformandoDadosDeUmCursoValidoEUmUsuarioAutenticado_DeveRetornarSucesso()
+        public async Task Registrar_InformandoDadosDeUmCursoValidoEUmUsuarioAutenticado_DeveRetornarSucesso()
         {
             // Arrange
-            CursoViewModelInput = new AutoFaker<CursoViewModelInput>(AutoBogusConfiguration.LOCATE);
+            var cursoViewModelInput = new AutoFaker<CursoViewModelInput>();
 
-            StringContent content = new StringContent(JsonConvert.SerializeObject(CursoViewModelInput), Encoding.UTF8,
-              "application/json");
+            StringContent content = new StringContent(JsonConvert.SerializeObject(cursoViewModelInput.Generate()), Encoding.UTF8, "application/json");
 
             // Act
             _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", LoginViewModelOutput.Token);
-            var httpClientRequest = await _httpClient.PostAsync("/api/v1/curso/cadastro", content);
+            var httpClientRequest = await _httpClient.PostAsync("api/v1/curso/cadastro", content);
 
-            // Assert          
-            Assert.Equal(System.Net.HttpStatusCode.Created, httpClientRequest.StatusCode);
+            // Assert
+            Assert.Equal(HttpStatusCode.Created, httpClientRequest.StatusCode);
         }
 
         [Fact]
-        public async Task Cadastrar_InformandoDadosDeUmCursoValidoEUmUsuarioNaoAutenticado_DeveRetornarSucesso()
+        public async Task Registrar_InformandoDadosDeUmCursoValidoEUmUsuarioNaoAutenticado_DeveRetornarSucesso()
         {
             // Arrange
-            CursoViewModelInput = new AutoFaker<CursoViewModelInput>(AutoBogusConfiguration.LOCATE);
-
-            StringContent content = new StringContent(JsonConvert.SerializeObject(CursoViewModelInput), Encoding.UTF8,
-              "application/json");
+            var cursoViewModelInput = new AutoFaker<CursoViewModelInput>();
+            StringContent content = new StringContent(JsonConvert.SerializeObject(cursoViewModelInput.Generate()), Encoding.UTF8, "application/json");
 
             // Act
-            var httpClientRequest = await _httpClient.PostAsync("/api/v1/curso/cadastro", content);
+            var httpClientRequest = await _httpClient.PostAsync("api/v1/curso/cadastro", content);
 
-            // Assert          
-            Assert.Equal(System.Net.HttpStatusCode.Unauthorized, httpClientRequest.StatusCode);
+            // Assert
+            Assert.Equal(HttpStatusCode.Unauthorized, httpClientRequest.StatusCode);
         }
-        
+
+
         [Fact]
-        public async Task Listar_InformandoUmUsuarioAutenticado_DeveRetornarSucesso()
+        public async Task Obter_InformandoUmUsuarioAutenticado_DeveRetornarSucesso()
         {
             // Arrange
-            await Cadastrar_InformandoDadosDeUmCursoValidoEUmUsuarioAutenticado_DeveRetornarSucesso();
+            await Registrar_InformandoDadosDeUmCursoValidoEUmUsuarioAutenticado_DeveRetornarSucesso();
 
             // Act
             _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", LoginViewModelOutput.Token);
-            var httpClientRequest = await _httpClient.GetAsync("/api/v1/curso/listarCursos");
+            var httpClientRequest = await _httpClient.GetAsync("api/v1/curso/listarCursos");
 
             // Assert
             var cursos = JsonConvert.DeserializeObject<IList<CursoViewModelOutput>>(await httpClientRequest.Content.ReadAsStringAsync());
-
-            Assert.NotEmpty(cursos);
-            Assert.Equal(System.Net.HttpStatusCode.OK, httpClientRequest.StatusCode);
-        }
-
-        [Fact]
-        public async Task Listar_InformandoUmUsuarioNaoAutenticado_DeveRetornarSucesso()
-        {
-            // Arrange
-            await Cadastrar_InformandoDadosDeUmCursoValidoEUmUsuarioAutenticado_DeveRetornarSucesso();
-
-            // Act
-            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "");
-            var httpClientRequest = await _httpClient.GetAsync("/api/v1/curso/listarCursos");
-
-            // Assert
-            var cursos = JsonConvert.DeserializeObject<IList<CursoViewModelOutput>>(await httpClientRequest.Content.ReadAsStringAsync());
-
-            Assert.Equal(System.Net.HttpStatusCode.Unauthorized, httpClientRequest.StatusCode);
+            Assert.NotNull(cursos);
+            Assert.Equal(HttpStatusCode.OK, httpClientRequest.StatusCode);
         }
     }
 }
--- tests/curso.api.tests/Integrations/Controllers/UsuarioControllerTests.cs
+++ tests/curso.api.tests/Integrations/Controllers/UsuarioControllerTests.cs
@@ -1,78 +1,78 @@
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
 using AutoBogus;
 using curso.api.Models.Usuarios;
 using curso.api.tests.Configurations;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Newtonsoft.Json;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace curso.api.tests.Integrations.Controllers
 {
-  public class UsuarioControllerTests : IClassFixture<WebApplicationFactory<Startup>>, IAsyncLifetime
-  {
-    private readonly WebApplicationFactory<Startup> _factory;
-    protected readonly HttpClient _httpClient;
-    protected RegistroViewModelInput RegistroViewModelInput;
-    protected LoginViewModelOutput LoginViewModelOutput;
-
-    public UsuarioControllerTests(WebApplicationFactory<Startup> factory)
+    public class UsuarioControllerTests : IClassFixture<WebApplicationFactory<Startup>>, IAsyncLifetime
     {
-      _factory = factory;
-      _httpClient = _factory.CreateClient();
+        private readonly WebApplicationFactory<Startup> _factory;
+        protected readonly HttpClient _httpClient;
+        protected RegistroViewModelInput RegistroViewModelInput;
+        protected LoginViewModelOutput LoginViewModelOutput;
+
+        public UsuarioControllerTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+            _httpClient = _factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task Registrar_InformandoUsuarioESenha_DeveRetornarSucesso()
+        {
+            // Arrange
+            RegistroViewModelInput = new AutoFaker<RegistroViewModelInput>(AutoBogusConfiguration.LOCATE)
+                                            .RuleFor(p => p.Login, faker => faker.Person.UserName)
+                                            .RuleFor(p => p.Email, faker => faker.Person.Email);
+
+            StringContent content = new StringContent(JsonConvert.SerializeObject(RegistroViewModelInput), Encoding.UTF8, "application/json");
+
+            // Act
+            var httpClientRequest = await _httpClient.PostAsync("api/v1/usuario/registrar", content);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.Created, httpClientRequest.StatusCode);
+        }
+
+        [Fact]
+        public async Task Logar_InformandoUsuarioESenhaExistentes_DeveRetornarSucesso()
+        {
+            // Arrange
+            var loginViewModelInput = new LoginViewModelInput
+            {
+                Login = RegistroViewModelInput.Login,
+                Senha = RegistroViewModelInput.Senha
+            };
+            StringContent content = new StringContent(JsonConvert.SerializeObject(loginViewModelInput), Encoding.UTF8, "application/json");
+
+            // Act
+            var httpClientRequest = await _httpClient.PostAsync("api/v1/usuario/logar", content);
+
+            LoginViewModelOutput = JsonConvert.DeserializeObject<LoginViewModelOutput>(await httpClientRequest.Content.ReadAsStringAsync());
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, httpClientRequest.StatusCode);
+            Assert.NotNull(LoginViewModelOutput.Token);
+            Assert.Equal(loginViewModelInput.Login, LoginViewModelOutput.Usuario.Login);
+        }
+
+        public async Task InitializeAsync()
+        {
+            await Registrar_InformandoUsuarioESenha_DeveRetornarSucesso();
+            await Logar_InformandoUsuarioESenhaExistentes_DeveRetornarSucesso();
+        }
+
+        public async Task DisposeAsync()
+        {
+            _httpClient.Dispose();
+        }
     }
-
-    [Fact]
-    public async Task Registrar_InformandoUsuarioESenha_DeveRetornarSucesso()
-    {
-      // Arrange
-      RegistroViewModelInput = new AutoFaker<RegistroViewModelInput>(AutoBogusConfiguration.LOCATE)
-                                        .RuleFor(p => p.Email, faker => faker.Person.Email);
-
-      StringContent content = new StringContent(JsonConvert.SerializeObject(RegistroViewModelInput), Encoding.UTF8,
-        "application/json");
-
-      // Act
-      var httpClientRequest = await _httpClient.PostAsync("/api/v1/usuario/registrar", content);
-
-      // Assert
-      Assert.Equal(System.Net.HttpStatusCode.Created, httpClientRequest.StatusCode);
-    }
-
-    [Fact]
-    public async Task Logar_InformandoUsuarioESenhaExistentes_DeveRetornarSucesso()
-    {
-      // Arrange
-      var loginViewModelInput = new LoginViewModelInput
-      {
-        Login = RegistroViewModelInput.Login,
-        Senha = RegistroViewModelInput.Senha
-      };
-
-      StringContent content = new StringContent(JsonConvert.SerializeObject(loginViewModelInput), Encoding.UTF8,
-        "application/json");
-
-      // Act
-      var httpClientRequest = await _httpClient.PostAsync("/api/v1/usuario/logar", content);
-
-      LoginViewModelOutput = JsonConvert.DeserializeObject<LoginViewModelOutput>(await httpClientRequest.Content.ReadAsStringAsync());
-
-      // Assert
-      Assert.Equal(System.Net.HttpStatusCode.OK, httpClientRequest.StatusCode);
-      Assert.NotNull(LoginViewModelOutput.Token);
-      Assert.Equal(loginViewModelInput.Login, LoginViewModelOutput.Usuario.Login);
-    }
-
-    public async Task InitializeAsync()
-    {
-      await Registrar_InformandoUsuarioESenha_DeveRetornarSucesso();
-      await Logar_InformandoUsuarioESenhaExistentes_DeveRetornarSucesso();
-    }
-
-    public async Task DisposeAsync()
-    {
-      _httpClient.Dispose();
-    }
-  }
 }
